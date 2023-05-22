88c78601f9f1aa84d8f64680ac112e0621b49a24
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-05-22 12:56:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+//we want the user to be able to move the dodger to the left using the left arrow key
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+  //we want the user to be able to move the dodger to the right using the right arrow key
+  function moveDodgerRight() {
+   const leftNumbers = dodger.style.left.replace("px", "");
+   const left = parseInt(leftNumbers, 10);
+    const right = left + dodger.offsetWidth;
+    
+  
+    if (right > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
